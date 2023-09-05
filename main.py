c10b9285c4ab3514b09968b445886fe89604bb5e
Diasn1 <diasni@mail.ru>
2023-09-05 21:30:40 +0600

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# y = (lambda x : list(range(1, x+1)))(10)
-# print(y)
-# print(y[2])
-# y.append(55)
-# print(y)
+y = (lambda x : list(range(1, x+1)))(10)
+print(y)
+print(y[2])
+y.append(55)
+print(y)
 
